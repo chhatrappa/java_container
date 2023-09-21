38a49d0d390f842639b6873a05a0d22470d47cf6
Chhatrappa Chinnarathod <143782759+chhatrappa@users.noreply.github.com>
2023-09-21 14:32:27 +0000

    created a simple java file

---
+++ javaMain/main.java
@@ -0,0 +1,6 @@
+public class main {
+    // define a main function that prints "Hello World!" to the console
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
